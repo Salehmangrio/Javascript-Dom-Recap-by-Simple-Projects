7f49d3a790ab53d2694385cc8fed4097f1db1cb6
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2024-12-10 22:29:09 -0800

    FORM VALIDATION USING JS

--- FORM VALIDATION JS/script.js
+++ FORM VALIDATION JS/script.js
@@ -15,40 +15,39 @@ function submitedData() {
         alert("Please fill in all fields");
         return false;
     }
-
-
-    if(contact.length<10 || contact.length > 10) {
+    else if (contact.length < 10 || contact.length > 10) {
         alert("Contact number must be of 10 digits long.");
         return false;
     }
 
 
     //Checking if number consist any thing else then numbers
-    if(isNaN(contact)){
+    else if (isNaN(contact)) {
         alert("Contact number must contain only numbers.");
         return false;
     }
-    
+
     // Validate password length
-    if (password.length<8) {
+    else if (password.length < 8) {
         alert("Password must be at least 8 characters long.");
         return false;
     }
 
     // Validate password and confirm password mathes
-    if (password !== confirmPass) {
+    else if (password !== confirmPass) {
         alert("Passwords do not match");
         return false;
     }
 
-    
+
     // ALL DONE
-    
-    alert(
-        `Thank you for registering with us! 
+    else {
+        alert(
+            `Thank you for registering with us! 
         Your name is ${name}, 
         email is ${email},
         contact number is ${contact}, 
         and password is ${password}`
-    );
+        );
+    }
 }
